f7dd6555c7ef0877b4658c0d01582d7d70261f10
Karel Bilek <kb@karelbilek.com>
2024-03-19 13:13:31 +0100

    code dump

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    let number = 5;
+</script>
+
+<form action="/submit.html" method="post">
+    Value is {number}.<br>
+    <input type="number" bind:value="{number}" class="form-control" id="some_input"><br>
+    <button type="submit">Submit</button>
+</form>
