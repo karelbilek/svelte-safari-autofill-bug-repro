c064b76b3a8cd33dcdf8ae2ff7a4b0cb13a2da1c
Karel Bilek <kb@karelbilek.com>
2024-03-19 15:42:47 +0100

    more events

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,19 @@
     let number = 5;
 </script>
 
-<form action="/submit.html" method="post">
+<form action="/submit.html"
+    on:change={()=>console.log("form changed")}
+    on:update={()=>console.log("form updated")}
+    on:paste={()=>console.log("form pasted")}
+    on:input={()=>console.log("form inputed")}
+    method="post">
     Value is {number}.<br>
-    <input type="number" bind:value="{number}" class="form-control" id="some_input"><br>
+    <input type="number"
+        on:update={()=>console.log("updated")}
+        on:paste={()=>console.log("pasted")}
+        bind:value="{number}"
+        on:change={()=>console.log("changed")}
+        on:input={()=>console.log("inputed")}
+        class="form-control" id="some_input"><br>
     <button type="submit">Submit</button>
 </form>
